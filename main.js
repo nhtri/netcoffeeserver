9f687a13d33b2c3b6a650ae0a2921bd91e08c50a
Tri Nguyen <tri.nh@hotmail.com>
2019-12-12 23:55:27 +0700

    1

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ app.post('/wifi/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('INSERT INTO wifi VALUES ?', postData, function (error, results, fields) {
+    pool.query('INSERT INTO wifi VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

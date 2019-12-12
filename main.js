d287b896260241d25a0a3965bb5d9250ebe97554
Tri Nguyen <tri.nh@hotmail.com>
2019-12-12 23:38:48 +0700

    q

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ app.post('/wifi/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('INSERT INTO `wifi` SET ?', postData, function (error, results, fields) {
+    pool.query('INSERT INTO wifi SET ?', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
@@ -104,7 +104,7 @@ app.put('/wifi/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('UPDATE `wifi` SET `SDTSim`=?,`NgayThue`=?,`NgayTra`=?,`GiaCuoc`=? where `MaWiFi`=?', [req.body.SDTSim, req.body.NgayThue, req.body.NgayTra, req.body.GiaCuoc, req.body.MaWiFi], function (error, results, fields) {
+    pool.query('UPDATE wifi SET SDTSim=?,NgayThue=?,NgayTra=?,GiaCuoc=? where MaWiFi=?', [req.body.SDTSim, req.body.NgayThue, req.body.NgayTra, req.body.GiaCuoc, req.body.MaWiFi], function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

a96b0801140188d198604a564b0e30eca4b2cd61
Tri Nguyen <tri.nh@hotmail.com>
2019-12-12 13:59:07 +0700

    q

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ app.get('/wifi/old', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('select * from wifi where `TrangThai`=0', function (error, results, fields) {
+    pool.query('select * from wifi where TrangThai=0', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

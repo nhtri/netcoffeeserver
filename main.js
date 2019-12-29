5f950dbb1229764a9ab214474273902d9dff95f5
Tri Nguyen <tri.nh@hotmail.com>
2019-12-29 20:29:18 +0700

    .

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ app.delete('/wifi/:id', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('DELETE FROM wifi WHERE mawifi = ($1)', [id], function (error, results, fields) {
+    pool.query('DELETE FROM wifi WHERE mawifi = $1', [id], function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

d08f1e564dce31c223305310bec7eb2716fb978b
Tri Nguyen <tri.nh@hotmail.com>
2019-12-29 15:43:56 +0700

    del

--- main.js
+++ main.js
@@ -97,14 +97,14 @@ app.post('/wifi/', function (req, res) {
 });
 
 //rest api to delete a record in mysql database
-app.delete('/wifi/', function (req, res) {
-    var postData = req.body;
+app.delete('/wifi/:id', function (req, res) {
+    const id = parseInt(request.params.id)
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('DELETE FROM wifi WHERE mawifi = ($1)', postData, function (error, results, fields) {
+    pool.query('DELETE FROM wifi WHERE mawifi = ($1)', [id], function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

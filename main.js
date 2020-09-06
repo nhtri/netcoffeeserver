9cd66ac1f0cd81d4818229fa39faa1379aaddc69
tringuyenhuu <tri.nh@hotmail.com>
2020-09-06 15:11:48 +0700

    1

--- main.js
+++ main.js
@@ -215,7 +215,7 @@ app.put('/wifitamngung/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('UPDATE wifi SET trangthai_kh='tamngung' where mawifi=($1)', postData, function (error, results, fields) {
+    pool.query('UPDATE wifi SET trangthai_kh=($1) where mawifi=($2)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });

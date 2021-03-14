513a59c46d285d427c49ca175b41a59c46aee099
tringuyen <tri.nh@hotmail.com>
2021-03-14 13:02:33 +0700

    a

--- main.js
+++ main.js
@@ -266,7 +266,7 @@ app.put('/congtacvien/', function (req, res) {
 //rest api to authen
 app.get('/admin', cors(), function (req, res) {
     console.log(req);
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');

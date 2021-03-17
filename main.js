659ba5dd0cff114c7cc6af66c0c727489bf72a5b
tringuyen <tri.nh@hotmail.com>
2021-03-18 06:27:13 +0700

    up

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ var connectionString =
 //app.use(cors());
 //app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+//app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//  next();
+//});
 
 // connection.connect(function (err) {
 //     if (err) throw err

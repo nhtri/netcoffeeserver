26a283389afdc858225e3db89caf03c5eac4500b
tringuyen <tri.nh@hotmail.com>
2021-03-20 15:14:54 +0700

    up

--- main.js
+++ main.js
@@ -10,9 +10,6 @@ app.use(function(req, res, next) {
   // Mọi domain
   res.header("Access-Control-Allow-Origin", "*");
  
-  // Domain nhất định
-  // res.header("Access-Control-Allow-Origin", "https://freetuts.net");
- 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
